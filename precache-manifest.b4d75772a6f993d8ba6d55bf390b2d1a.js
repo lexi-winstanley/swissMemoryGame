c91c6c3b20784cf343ee1e7bf89739aa5529a0cb
lexi-winstanley <50344054+lexi-winstanley@users.noreply.github.com>
2019-10-09 23:47:55 -0700

    Updates

---
+++ precache-manifest.b4d75772a6f993d8ba6d55bf390b2d1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac4759f4cbbbff4f6397516a279c456",
+    "url": "/swissMemoryGame/index.html"
+  },
+  {
+    "revision": "e783e61862ecba318062",
+    "url": "/swissMemoryGame/static/css/main.c953b731.chunk.css"
+  },
+  {
+    "revision": "d84dfe67aa6f4c0cf283",
+    "url": "/swissMemoryGame/static/js/2.46ec92cf.chunk.js"
+  },
+  {
+    "revision": "e783e61862ecba318062",
+    "url": "/swissMemoryGame/static/js/main.9565cc30.chunk.js"
+  },
+  {
+    "revision": "a84951936f4115208624",
+    "url": "/swissMemoryGame/static/js/runtime-main.2dc008f5.js"
+  }
+]);
